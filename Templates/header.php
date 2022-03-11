d582c208a4d8520deb70b3d37f351b0cc86a9139
Reck171 <ikulikov345@gmail.com>
2022-03-11 22:45:56 +0300

    Шаблонизирование шапки и подвала, добавление шаблонов в "Контакты"

---
+++ Templates/header.php
@@ -0,0 +1,57 @@
+<header class="header">
+            <div class="container">
+                <div class="header-wrapper">
+                    <div class=header_name>
+                        <a href="#" class="company_name">
+                            <h1 class="name">
+                                Photo Studio
+                            </h1>
+                        </a>
+                    </div>
+                    <div class="header__menu">
+                        <nav>
+                            <ul class="header__menu_items">
+                                <li class="header__menu_item">
+                                    <a href="#">
+                                        <button>
+                                            Главная
+                                        </button>
+                                    </a>
+                                </li>
+                                <li class="header__menu_item">
+                                    <a href="#">
+                                        <button>
+                                            Наши Услуги
+                                        </button>
+                                    </a>
+                                </li>
+                                <li class="header__menu_item">
+                                    <a href="./Contacts.php">
+                                        <button>
+                                            Контакты
+                                        </button>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="links">
+                        <a href="#" class="links_img">
+                            <button>
+                                <img src="Assets/icon.png" alt="">
+                            </button>
+                        </a>
+                        <a href="#" class="links_img">
+                            <button>
+                                <img src="Assets/icon.png" alt="">
+                            </button>
+                        </a>
+                        <a href="#" class="links_img">
+                            <button>
+                                <img src="Assets/icon.png" alt="">
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require "Templates/header.php";
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Photo Studio</title>
+        <link rel="shortcut icon" href="icon.png">
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="./style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+
+        
+        
+    </body>
+<?php
+require "Templates/footer.php"
+?>
+</html>
+            
+            
