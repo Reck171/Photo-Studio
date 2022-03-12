56c19c0bbf7bccfea807d3eec3acb5fb1e6c2000
Reck171 <ikulikov345@gmail.com>
2022-03-12 21:10:29 +0300

    насрал в контакты

--- Contacts.php
+++ Contacts.php
@@ -17,9 +17,20 @@ require "Templates/header.php";
 
     <body>
         <div class="container">
-            <div class="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2252.6678501913875!2d37.74188693358961!3d55.62519498864408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab15954c7caf7%3A0xbe3eac7f11d31cbe!2z0YPQuy4g0JzRg9GB0Ysg0JTQttCw0LvQuNC70Y8sIDE1INC60L7RgNC_0YPRgSAxLCDQnNC-0YHQutCy0LAsIDExNTU4MA!5e0!3m2!1sru!2sru!4v1647096472789!5m2!1sru!2sru" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </div>    
+            <div class="jopa">
+                <div class="aboutus">
+                    <h1>О нас</h1>
+                    <p class="text">Физически фотостудия представляет собой помещение (комната, зал, павильон) без окон или с закрытыми (непрозрачным материалом) окнами, в котором созданы максимально удобные условия для полного управления светом, размещения снимаемого объекта и удобной работы фотографа.</p>
+                </div>
+                <div class="contacts">
+                    <h1>Наши контакты</h1>
+                    <p class="text">Тел:<a href="tell:+78005553535">+7 (800)-555-35-35<a></p>
+                    <p class="text">Почта:<a href="mailto:randommail@mail.ru">randommail@mail.ru<a></p>
+                </div>
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2252.6678501913875!2d37.74188693358961!3d55.62519498864408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab15954c7caf7%3A0xbe3eac7f11d31cbe!2z0YPQuy4g0JzRg9GB0Ysg0JTQttCw0LvQuNC70Y8sIDE1INC60L7RgNC_0YPRgSAxLCDQnNC-0YHQutCy0LAsIDExNTU4MA!5e0!3m2!1sru!2sru!4v1647096472789!5m2!1sru!2sru" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>    
+            </div>
         </div>    
     </body>
 <?php
